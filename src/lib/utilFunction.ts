887ddaf8ddb5e1d252a43df1aae1c2717b11e0f7
melonMochiRoll <soulhwasin@gmail.com>
2025-03-11 17:47:02 +0900

    :fire: 일부 함수 제거

--- src/lib/utilFunction.ts
+++ src/lib/utilFunction.ts
@@ -6,20 +6,6 @@ export const getByteSize = (str: string) => {
   return encoder.encode(str).length;
 };
 
-export const isOverlapping = (startA: string, endA: string, startB: string, endB: string) => {
-  const [ startA_hour, startA_minute ] = startA.split(':');
-  const [ endA_hour, endA_minute ] = endA.split(':');
-  const [ startB_hour, startB_minute ] = startB.split(':');
-  const [ endB_hour, endB_minute ] = endB.split(':');
-
-  const startA_date = dayjs().hour(Number(startA_hour)).minute(Number(startA_minute));
-  const endA_date = dayjs().hour(Number(endA_hour)).minute(Number(endA_minute));
-  const startB_date = dayjs().hour(Number(startB_hour)).minute(Number(startB_minute));
-  const endB_date = dayjs().hour(Number(endB_hour)).minute(Number(endB_minute));
-
-  return (startA_date < endB_date) && (startB_date < endA_date) ? true : false;
-};
-
 export const renderTime = (time: string) => {
   const [ hour, minute ] = time.split(':');
 
