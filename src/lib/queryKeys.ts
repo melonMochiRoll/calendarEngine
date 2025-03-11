503e75620373302c2f5ee9b15dbca279364b696e
melonMochiRoll <soulhwasin@gmail.com>
2025-03-11 17:16:02 +0900

    :sparkles: GET_TODOS_KEY 정의

--- src/lib/queryKeys.ts
+++ src/lib/queryKeys.ts
@@ -1,5 +1,6 @@
 
 export const GET_USER_KEY = 'getUser';
+export const GET_TODOS_KEY = 'getTodosByDate';
 export const GET_TODOS_LIST_KEY = 'getTodosForSpace';
 export const GET_SHAREDSPACE_KEY = 'getSharedspace';
 export const GET_SUBSCRIBED_SPACES_KEY = 'getSubscribedspaces';
