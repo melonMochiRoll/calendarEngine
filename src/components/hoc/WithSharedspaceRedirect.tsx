40806cb4e02e5592da606a022db6a8b6b93d474a
melonMochiRoll <soulhwasin@gmail.com>
2025-04-24 13:36:03 +0900

    :recycle: 페이지 경로 매직스트링 제거

--- src/components/hoc/WithSharedspaceRedirect.tsx
+++ src/components/hoc/WithSharedspaceRedirect.tsx
@@ -4,6 +4,7 @@ import useUser from 'Hooks/useUser';
 import { defaultToastOption, needLogin, privateTooltip, waitingMessage } from 'Lib/noticeConstants';
 import { toast } from 'react-toastify';
 import { useNavigate } from 'react-router-dom';
+import { PATHS } from 'Constants/paths';
 
 const WithSharedspaceRedirect = <P extends {}>(WrappedComponent: React.FunctionComponent<P>) => {
   return (props: P) => {
@@ -15,23 +16,23 @@ const WithSharedspaceRedirect = <P extends {}>(WrappedComponent: React.FunctionC
       if (!isLoading && error) {
         const response = {
           message: waitingMessage,
-          destination: '/internal',
+          destination: PATHS.INTERNAL,
         };
         
         if (errorCode === 403 && isLogin) {
           response.message = privateTooltip;
-          response.destination = '/forbidden';
+          response.destination = PATHS.FORBIDDEN;
         }
   
         if (errorCode === 403 && isNotLogin) {
           response.message = needLogin;
-          response.destination = '/login';
+          response.destination = PATHS.LOGIN;
         }
   
         toast.error(response.message, {
           ...defaultToastOption,
         });
-        navigate(`${response.destination}`);
+        navigate(response.destination);
       }
     }, [error]);
       
