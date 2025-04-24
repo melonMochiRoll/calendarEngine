0bbbb65062f9b6d32b90a3afe5271f07b894fa66
melonMochiRoll <soulhwasin@gmail.com>
2025-04-24 13:28:39 +0900

    :recycle: SharedspacePage로 경로 및 이름 변경

--- src/pages/SharedspacesPage.tsx
+++ src/pages/SharedspacesPage.tsx
@@ -1,93 +1,31 @@
-import React, { useEffect } from 'react';
+import React, { FC } from 'react';
 import styled from '@emotion/styled';
-import { Outlet, useMatches, useNavigate } from 'react-router-dom';
-import Sidebar from 'Containers/Sidebar';
-import SharedspaceHeader from 'Containers/SharedspaceHeader';
-import SkeletonSharedspaceHeader from 'Components/skeleton/SkeletonSharedspaceHeader';
-import TodoContainer from 'Containers/TodoContainer';
-import { defaultToastOption, needLogin, privateTooltip, waitingMessage } from 'Lib/noticeConstants';
-import { toast } from 'react-toastify';
-import useSharedspace from 'Hooks/useSharedspace';
-import useUser from 'Hooks/useUser';
+import Header from 'Containers/Header';
+import SubscribedSpacesContainer from 'Containers/SubscribedSpacesContainer';
+import WithAuthGuard from 'Components/hoc/WithAuthGuard';
 
-const SharedspacesPage = () => {
-  const navigate = useNavigate();
-  const matches = useMatches();
-
-  const { isLogin, isNotLogin } = useUser();
-
-  const {
-    data: spaceData,
-    error,
-    errorCode,
-    isLoading,
-  } = useSharedspace();
-
-  useEffect(() => {
-    if (error) {
-      const response = {
-        message: waitingMessage,
-        destination: '/internal',
-      };
-      
-      if (errorCode === 403 && isLogin) {
-        response.message = privateTooltip;
-        response.destination = '/forbidden';
-      }
-
-      if (errorCode === 403 && isNotLogin) {
-        response.message = needLogin;
-        response.destination = '/login';
-      }
-
-      toast.error(response.message, {
-        ...defaultToastOption,
-      });
-      navigate(`${response.destination}`);
-    }
-  }, [error]);
-  
-  useEffect(() => {
-    if (matches.length < 2) {
-      navigate('/sharedspaces/subscribed');
-    }
-  }, [matches]);
-  
+const SharedspacesPage: FC = () => {
   return (
     <Block>
-      <Sidebar />
-      <Content>
-        {
-          !isLoading && spaceData ?
-            <SharedspaceHeader
-              spaceData={spaceData} /> :
-            <SkeletonSharedspaceHeader />
-        }
-        <Main>
-          <Outlet />
-          <TodoContainer />
-        </Main>
-      </Content>
+      <Header />
+      <Main>
+        <SubscribedSpacesContainer />
+      </Main>
     </Block>
   );
 };
 
-export default SharedspacesPage;
+export default WithAuthGuard(SharedspacesPage);
 
 const Block = styled.div`
   display: flex;
-`;
-
-const Content = styled.div`
-  display: flex;
   flex-direction: column;
-  width: 100%;
   height: 100vh;
   background-color: var(--black);
 `;
 
 const Main = styled.main`
   display: flex;
-  justify-content: space-between;
-  height: 95%;
+  flex-direction: column;
+  height: 100%;
 `;
