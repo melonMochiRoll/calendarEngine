4c685f780a4736ebdeef9774aac722f38da459f9
melonMochiRoll <soulhwasin@gmail.com>
2025-04-24 13:11:53 +0900

    :sparkles: PATHS 정의

---
+++ src/constants/paths.ts
@@ -0,0 +1,11 @@
+export const PATHS = {
+  HOME: '/',
+  LOGIN: '/login',
+  JOIN: '/join',
+  SHAREDSPACE: '/sharedspaces',
+  SHAREDSPACE_VIEW: '/sharedspaces/view',
+  SHAREDSPACE_CHAT: '/sharedspaces/chat',
+  FORBIDDEN: '/forbidden',
+  NOTFOUND: '/not-found',
+  INTERNAL: '/internal',
+};
