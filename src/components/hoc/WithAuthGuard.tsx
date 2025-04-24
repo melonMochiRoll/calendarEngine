02136b0ab41d157edca3641d1247dfea71e940b8
melonMochiRoll <soulhwasin@gmail.com>
2025-04-24 13:31:03 +0900

    :sparkles: WithAuthGuard 정의

---
+++ src/components/hoc/WithAuthGuard.tsx
@@ -0,0 +1,36 @@
+import React, { useEffect } from 'react';
+import useUser from 'Hooks/useUser';
+import { defaultToastOption, needLogin } from 'Lib/noticeConstants';
+import { toast } from 'react-toastify';
+import { useNavigate } from 'react-router-dom';
+import { PATHS } from 'Constants/paths';
+
+const WithAuthGuard = <P extends {}>(WrappedComponent: React.FunctionComponent<P>) => {
+  return (props: P) => {
+    const navigate = useNavigate();
+    const { userData, isNotLogin } = useUser();
+
+    useEffect(() => {
+      if (isNotLogin) {
+        const delay = setTimeout(() => {
+          if (userData) {
+            clearTimeout(delay);
+          } else {
+            toast.error(needLogin, {
+              ...defaultToastOption,
+            });
+            navigate(PATHS.LOGIN);
+          }
+        }, 500);
+
+        return () => {
+          clearTimeout(delay);
+        };
+      }
+    }, [userData, isNotLogin]);
+      
+    return <WrappedComponent {...props} />;
+  }
+}
+
+export default WithAuthGuard;
