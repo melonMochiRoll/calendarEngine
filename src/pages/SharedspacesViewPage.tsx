373ca0abb36753aab5e2470ee7dc32c4f05bf916
melonMochiRoll <soulhwasin@gmail.com>
2025-04-29 21:36:08 +0900

    :recycle: AsyncBoundary 적용 및 가독성 개선

--- src/pages/SharedspacesViewPage.tsx
+++ src/pages/SharedspacesViewPage.tsx
@@ -1,10 +1,38 @@
 import React, { FC } from 'react';
-import CalendarContainer from 'Containers/CalendarContainer';
+import styled from '@emotion/styled';
+import CalendarCreator from 'Containers/CalendarCreator';
+import CalendarHeader from 'Components/calendar/CalendarHeader';
+import AsyncBoundary from 'Components/AsyncBoundary';
+import { GET_TODOS_LIST_KEY } from 'Lib/queryKeys';
+import LoadingCircular from 'Components/skeleton/LoadingCircular';
+import GenericErrorFallback from 'Components/errors/GenericErrorFallback';
+import { useQueryClient } from '@tanstack/react-query';
 
 const SharedspacesViewPage: FC = () => {
+  const qc = useQueryClient();
+
   return (
-    <CalendarContainer />
+    <Calendar>
+      <CalendarHeader />
+      <AsyncBoundary
+        errorBoundaryFallback={GenericErrorFallback}
+        suspenseFallback={<LoadingCircular />}
+        onReset={() => {
+          qc.removeQueries([GET_TODOS_LIST_KEY]);
+        }}>
+        <CalendarCreator />
+      </AsyncBoundary>
+    </Calendar>
   );
 };
 
 export default SharedspacesViewPage;
+
+const Calendar = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 70%;
+  height: 100%;
+  padding: 20px 80px;
+  background-color: var(--black);
+`;
