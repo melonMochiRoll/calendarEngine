395abd7a2cab1a66f9cac37638170b4d78644101
melonMochiRoll <soulhwasin@gmail.com>
2025-04-29 14:18:26 +0900

    :art: 기능 확장

--- src/components/AsyncBoundary.tsx
+++ src/components/AsyncBoundary.tsx
@@ -1,31 +1,33 @@
-import React, { FC, Suspense } from 'react';
+import React, { ErrorInfo, FC, Suspense } from 'react';
 import { ErrorBoundary, FallbackProps } from 'react-error-boundary';
 
 interface AsyncBoundaryProps {
-  errorBoundaryFallback?: React.ComponentType<FallbackProps>;
+  errorBoundaryFallback: (errors: FallbackProps, errorRenderComponent?: React.ReactNode) => React.ReactNode;
   errorComponent?: React.ReactNode,
   suspenseFallback: React.ReactNode;
+  onError?: (error: Error, info: ErrorInfo) => void;
+  onReset?: (
+    details:
+      | { reason: "imperative-api"; args: any[] }
+      | { reason: "keys"; prev: any[] | undefined; next: any[] | undefined }
+  ) => void;
+  errorRenderComponent?: React.ReactNode;
   children: React.ReactNode;
 };
 
 const AsyncBoundary: FC<AsyncBoundaryProps> = ({
-  errorBoundaryFallback = () => null,
-  errorComponent = null,
+  errorBoundaryFallback,
   suspenseFallback,
+  onError,
+  onReset,
+  errorRenderComponent,
   children,
 }) => {
-  if (errorComponent) {
-    return (
-      <ErrorBoundary fallback={errorComponent}>
-        <Suspense fallback={suspenseFallback}>
-          {children}
-        </Suspense>
-      </ErrorBoundary>
-    );
-  }
-  
   return (
-    <ErrorBoundary FallbackComponent={errorBoundaryFallback}>
+    <ErrorBoundary
+      FallbackComponent={(error) => errorBoundaryFallback(error, errorRenderComponent)}
+      onError={onError}
+      onReset={onReset}>
       <Suspense fallback={suspenseFallback}>
         {children}
       </Suspense>
