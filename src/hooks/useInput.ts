ac399fd1a9b34533cdb9c7b57487a9d95a0f2216
melonMochiRoll <soulhwasin@gmail.com>
2025-05-12 20:58:17 +0900

    :label: 이벤트 객체 타이핑

--- src/hooks/useInput.ts
+++ src/hooks/useInput.ts
@@ -3,8 +3,8 @@ import { useCallback, useState } from "react";
 const useInput = <T>(initData: T) => {
   const [ state, setState ] = useState(initData);
   
-  const onChangeState = useCallback((e: any) => {
-    setState(e.target.value);
+  const onChangeState = useCallback((e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+    setState(e.target.value as T);
   }, [state]);
 
   return [ state, onChangeState, setState ] as const;
