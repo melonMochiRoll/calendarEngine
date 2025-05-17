d716ca83731483781350d9d0ec54154babd2e2d5
melonMochiRoll <soulhwasin@gmail.com>
2025-05-17 10:37:16 +0900

    :recycle: 훅스 구조 개선

--- src/hooks/useSearchUsers.ts
+++ src/hooks/useSearchUsers.ts
@@ -1,53 +1,28 @@
 import { useQuery } from "@tanstack/react-query";
-import { useAppSelector } from "./reduxHooks";
 import { TSearchUsers } from "Typings/types";
 import { searchUsers } from "Api/usersApi";
-import { useEffect } from "react";
 import { SEARCH_USERS_KEY } from "Constants/queryKeys";
 
-type TypeSafeReturnType = {
+type UseSearchUsersReturnType = {
   data: TSearchUsers[];
 };
 
-type FetchStateReturnType = {
-  data: TSearchUsers[] | undefined;
-  isLoading: boolean;
-  error: unknown;
-};
-
-function useSearchUsers(options: { suspense: true, throwOnError: true }): TypeSafeReturnType;
-function useSearchUsers(options?: { suspense: boolean, throwOnError: boolean }): FetchStateReturnType;
-
-function useSearchUsers(options = { suspense: false, throwOnError: false }) {
-  const { query } = useAppSelector(state => state.searchUsers);
-  const { suspense, throwOnError } = options;
-
+export function useSearchUsers(query: string): UseSearchUsersReturnType {
   const {
     data,
     isLoading,
-    refetch,
     error,
   } = useQuery<TSearchUsers[]>({
-    queryKey: [SEARCH_USERS_KEY],
+    queryKey: [SEARCH_USERS_KEY, query],
     queryFn: () => searchUsers(query),
     refetchOnWindowFocus: false,
-    suspense,
-    useErrorBoundary: throwOnError,
+    suspense: true,
+    useErrorBoundary: true,
   });
 
-  useEffect(() => {
-    refetch();
-  }, [query]);
+  if (isLoading) throw new Promise(() => {});
+  if (error) throw error;
+  if (!data) throw new Error();
 
-  if (suspense) {
-    if (isLoading) throw new Promise(() => {});
-    if (error) throw error;
-    if (!data) throw new Error();
-
-    return { data };
-  }
-
-  return { data, isLoading, error };
+  return { data };
 }
-
-export default useSearchUsers;
