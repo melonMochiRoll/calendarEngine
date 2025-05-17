55fac36d3f8e216a2af27ee3256b9a209caa6560
melonMochiRoll <soulhwasin@gmail.com>
2025-05-17 10:41:27 +0900

    :fire: NestedModal 삭제

--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -3,7 +3,6 @@ import { Outlet } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 import "react-toastify/dist/ReactToastify.css";
 import RenderModal from 'Components/modal/RenderModal';
-import NestedModal from 'Components/modal/NestedModal';
 
 const Layout: FC = () => {
   return (
@@ -11,7 +10,6 @@ const Layout: FC = () => {
       <Outlet />
       <ToastContainer />
       <RenderModal />
-      <NestedModal />
     </>
   );
 };
