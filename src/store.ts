48bc0c8ea4c30ee3a988f09e00208338af20bdf1
melonMochiRoll <soulhwasin@gmail.com>
2025-05-20 19:13:49 +0900

    :fire: todoDetailSlice 삭제

--- src/store.ts
+++ src/store.ts
@@ -2,7 +2,6 @@ import { configureStore } from "@reduxjs/toolkit";
 import modalReducer from 'Features/modalSlice';
 import calendarTimeReducer from 'Features/calendarTimeSlice';
 import todoTimeReducer from 'Features/todoTimeSlice';
-import todoDetailReducer from "Features/todoDetailSlice";
 import joinRequestDetailReducer from 'Features/joinRequestDetailSlice';
 
 export const reduxStore = configureStore({
@@ -10,7 +9,6 @@ export const reduxStore = configureStore({
     modal: modalReducer,
     calendarTime: calendarTimeReducer,
     todoTime: todoTimeReducer,
-    todoDetail: todoDetailReducer,
     joinRequestDetail: joinRequestDetailReducer,
   },
 })
