3d3ea1ec5a00d98e2496080dbd96ef6eecf2aad4
melonMochiRoll <soulhwasin@gmail.com>
2025-05-26 17:38:57 +0900

    :recycle: useTodosList, 인자 제거

--- src/containers/CalendarContainer.tsx
+++ src/containers/CalendarContainer.tsx
@@ -1,6 +1,6 @@
 import React, { FC } from 'react';
 import styled from '@emotion/styled';
-import useTodosList from 'Hooks/queries/useTodosList';
+import { useTodosList } from 'Hooks/queries/useTodosList';
 import DaysOfWeekHeader from 'Components/calendar/DaysOfWeekHeader';
 import CalendarDates from 'Components/calendar/CalendarDates';
 import { useAppSelector } from 'Hooks/reduxHooks';
@@ -8,7 +8,7 @@ import { useAppSelector } from 'Hooks/reduxHooks';
 interface CalendarContainerProps {};
 
 const CalendarContainer: FC<CalendarContainerProps> = () => {
-  const { data: todosListData } = useTodosList({ suspense: true, throwOnError: true });
+  const { data: todosListData } = useTodosList();
   const {
     calendarYear,
     calendarMonth,
