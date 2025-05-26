ad02c44c19e74536cac38a581cf38b55cf455497
melonMochiRoll <soulhwasin@gmail.com>
2025-05-26 18:00:45 +0900

    :recycle: refetch 방식 개선 및 suspense, useErrorBoundary 옵션 true 고정

--- src/hooks/queries/useJoinRequest.ts
+++ src/hooks/queries/useJoinRequest.ts
@@ -1,59 +1,33 @@
 import { useQuery } from "@tanstack/react-query";
 import { getJoinRequest } from "Api/joinrequestApi";
 import { GET_JOINREQUEST_KEY } from "Constants/queryKeys";
-import { useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { TJoinRequest } from "Typings/types";
 
-type TypeSafeReturnType = {
+type UseJoinRequestReturnType = {
   data: TJoinRequest[];
 };
 
-type FetchStateReturnType = {
-  data: TJoinRequest[] | undefined;
-  isLoading: boolean;
-  error: unknown;
-};
-
-function useJoinRequest(options: { suspense: true, throwOnError: true }): TypeSafeReturnType;
-function useJoinRequest(options?: { suspense: boolean, throwOnError: boolean }): FetchStateReturnType;
-
-function useJoinRequest(options = { suspense: false, throwOnError: false }) {
+export function useJoinRequest(): UseJoinRequestReturnType {
   const { url: _url } = useParams();
-  const { suspense, throwOnError } = options;
 
   const {
     data,
-    refetch,
     isLoading,
     error,
   } = useQuery<TJoinRequest[]>({
-    queryKey: [GET_JOINREQUEST_KEY],
+    queryKey: [GET_JOINREQUEST_KEY, _url],
     queryFn: () => getJoinRequest(_url),
     refetchOnWindowFocus: false,
-    suspense,
-    useErrorBoundary: throwOnError,
+    suspense: true,
+    useErrorBoundary: true,
   });
 
-  useEffect(() => {
-    refetch();
-  }, [_url]);
-
-  if (suspense) {
-    if (isLoading) throw new Promise(() => {});
-    if (error) throw error;
-    if (!data) throw new Error();
-
-    return {
-      data,
-    };
-  }
+  if (isLoading) throw new Promise(() => {});
+  if (error) throw error;
+  if (!data) throw new Error();
 
   return {
     data,
-    isLoading,
-    error,
   };
 }
-
-export default useJoinRequest;
