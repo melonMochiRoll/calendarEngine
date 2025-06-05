16b7f6da611382ad64c8427c08d6c34fff3fe73f
melonMochiRoll <soulhwasin@gmail.com>
2025-06-05 14:58:19 +0900

    :recycle: refetch 방식 개선 및 suspense, useErrorBoundary 옵션 true 고정

--- src/hooks/queries/useChats.ts
+++ src/hooks/queries/useChats.ts
@@ -1,68 +1,33 @@
 import { useQuery } from "@tanstack/react-query";
 import { getSharedspaceChats } from "Api/sharedspacesApi";
 import { GET_SHAREDSPACE_CHATS_KEY } from "Constants/queryKeys";
-import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { TChats } from "Typings/types";
 
-type TypeSafeReturnType = {
+type UseChatsReturnType = {
   data: TChats;
-  offset: number;
-  setOffset: React.Dispatch<React.SetStateAction<number>>;
 };
 
-type FetchStateReturnType = {
-  data: TChats | undefined;
-  offset: number;
-  setOffset: React.Dispatch<React.SetStateAction<number>>;
-  isLoading: boolean;
-  error: unknown;
-};
-
-function useChats(options: { suspense: true, throwOnError: true }): TypeSafeReturnType;
-function useChats(options?: { suspense: boolean, throwOnError: boolean }): FetchStateReturnType;
-
-function useChats(options = { suspense: false, throwOnError: false }) {
+export function useChats(offset: number): UseChatsReturnType {
   const { url: _url } = useParams();
-  const { suspense, throwOnError } = options;
-  const [ offset, setOffset ] = useState(1);
 
   const {
     data,
     isLoading,
-    refetch,
     error,
   } = useQuery<TChats>({
-    queryKey: [GET_SHAREDSPACE_CHATS_KEY],
+    queryKey: [GET_SHAREDSPACE_CHATS_KEY, _url],
     queryFn: () => getSharedspaceChats(_url, offset),
     refetchOnWindowFocus: false,
-    suspense,
-    useErrorBoundary: throwOnError,
+    suspense: true,
+    useErrorBoundary: true,
   });
 
-  useEffect(() => {
-    refetch();
-  }, [_url]);
-
-  if (suspense) {
-    if (isLoading) throw new Promise(() => {});
-    if (error) throw error;
-    if (!data) throw new Error();
-
-    return {
-      data,
-      offset,
-      setOffset,
-    };
-  }
+  if (isLoading) throw new Promise(() => {});
+  if (error) throw error;
+  if (!data) throw new Error();
 
   return {
     data,
-    offset,
-    setOffset,
-    isLoading,
-    error,
   };
 }
-
-export default useChats;
