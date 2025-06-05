26878a8505824e5388d225a79ac27fbf76ad7fe5
melonMochiRoll <soulhwasin@gmail.com>
2025-06-05 13:29:19 +0900

    :recycle: handleRetry, 재사용성 개선

--- src/lib/utilFunction.ts
+++ src/lib/utilFunction.ts
@@ -53,11 +53,10 @@ export const formatDate = (date: string) => {
   return `${splited[0]}년 ${splited[1]}월 ${splited[2]}일`;
 };
 
-export const handleRetry = (failureCount: number, error: unknown) => {
+export const handleRetry = (noRetryStatusCodes: number[], failureCount: number, error: unknown) => {
   const errorCode = axios.isAxiosError(error) && error?.response?.status || 500;
-  const retryDisableCodes = [ 400, 401, 403, 404 ];
 
-  if (retryDisableCodes.includes(errorCode)) {
+  if (noRetryStatusCodes.includes(errorCode)) {
     return false;
   }
 
