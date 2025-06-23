438cb62c661d914d4b70c8fad7873429bdf2e036
melonMochiRoll <soulhwasin@gmail.com>
2025-06-23 18:46:23 +0900

    :lipstick: UI 구조 개선

--- src/components/skeleton/SkeletonChatList.tsx
+++ src/components/skeleton/SkeletonChatList.tsx
@@ -1,4 +1,5 @@
 import React, { FC } from 'react';
+import styled from '@emotion/styled';
 import SkeletonChat from './SkeletonChat';
 
 const SkeletonChatList: FC = () => {
@@ -38,17 +39,48 @@ const SkeletonChatList: FC = () => {
   ];
 
   return (
-    <>
-    {
-      skeleton.map((v: typeof skeleton[0], idx: number) => {
-        return <SkeletonChat
-          key={idx}
-          email={v.email}
-          content={v.content} />
-      })
-    }
-    </>
+    <ChatBlock>
+      <ChatWrapper>
+        <List>
+        {
+          skeleton.map((v: typeof skeleton[0], idx: number) => {
+            return <SkeletonChat
+              key={idx}
+              email={v.email}
+              content={v.content} />
+          })
+        }
+        </List>
+      </ChatWrapper>
+    </ChatBlock>
   );
 };
 
 export default SkeletonChatList;
+
+const ChatBlock = styled.div`
+  display: flex;
+  justify-content: center;
+  width: 70%;
+`;
+
+const ChatWrapper = styled.div`
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  width: 1000px;
+  height: 100%;
+  background-color: var(--dark-gray);
+`;
+
+const List = styled.ul`
+  display: flex;
+  flex-direction: column-reverse;
+  width: 100%;
+  height: 80%;
+  padding: 0;
+  padding-bottom: 30px;
+  margin: 0;
+  gap: 20px;
+  overflow-y: scroll;
+`;
