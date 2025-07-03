cb4f3809174c046e11b2618de94114494432adb3
melonMochiRoll <soulhwasin@gmail.com>
2025-07-03 18:39:34 +0900

    :necktie: x-csrf-token 인터셉터 정의

--- src/api/axiosInstance.ts
+++ src/api/axiosInstance.ts
@@ -1,8 +1,21 @@
 import axios from "axios";
 import { getOrigin } from "Lib/utilFunction";
+import { getCsrfTokenFromStore } from "Src/store";
 
 export const axiosInstance = axios.create({
   baseURL: getOrigin(),
   headers: { "Content-Type" : "application/json" },
   withCredentials: true,
+});
+
+axiosInstance.interceptors.request.use(config => {
+  const token = getCsrfTokenFromStore();
+
+  if (token) {
+    config.headers['x-csrf-token'] = token;
+  }
+  
+  return config;
+}, error => {
+  throw error;
 });
