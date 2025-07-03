2276b9a527b57fb4fe0a6481c885395953a74802
melonMochiRoll <soulhwasin@gmail.com>
2025-07-03 18:38:11 +0900

    :heavy_plus_sign::sparkles: csrfTokenReducer 추가 및 getCsrfTokenFromStore 정의

--- src/store.ts
+++ src/store.ts
@@ -3,6 +3,7 @@ import modalReducer from 'Features/modalSlice';
 import calendarTimeReducer from 'Features/calendarTimeSlice';
 import todoTimeReducer from 'Features/todoTimeSlice';
 import globalErrorReducer from 'Features/globalErrorSlice';
+import csrfTokenReducer from 'Features/csrfTokenSlice';
 
 export const reduxStore = configureStore({
   reducer: {
@@ -10,8 +11,15 @@ export const reduxStore = configureStore({
     calendarTime: calendarTimeReducer,
     todoTime: todoTimeReducer,
     globalError: globalErrorReducer,
+    csrfToken: csrfTokenReducer,
   },
-})
+});
 
 export type RootState = ReturnType<typeof reduxStore.getState>;
 export type AppDispatch = typeof reduxStore.dispatch;
+
+export const getCsrfTokenFromStore = () => {
+  const state = reduxStore.getState();
+
+  return state.csrfToken.token;
+};
