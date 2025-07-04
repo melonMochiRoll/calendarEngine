9ea3f7ff763875eb4fe099bd1a3604a9f9f72ba1
melonMochiRoll <soulhwasin@gmail.com>
2025-07-04 19:16:53 +0900

    :necktie: useCsrfToken 적용

--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -3,8 +3,11 @@ import { Outlet } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 import "react-toastify/dist/ReactToastify.css";
 import RenderModal from 'Components/modal/RenderModal';
+import { useCsrfToken } from 'Src/hooks/queries/useCSRFToken';
 
 const Layout: FC = () => {
+  useCsrfToken();
+
   return (
     <>
       <Outlet />
