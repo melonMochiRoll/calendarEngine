c4c559171ceee709a1c13d9b66c0a2259d0142b8
melonMochiRoll <soulhwasin@gmail.com>
2025-08-01 20:06:26 +0900

    :sparkles: ModalLoadingCircular 정의

---
+++ src/components/skeleton/ModalLoadingCircular.tsx
@@ -0,0 +1,25 @@
+import React, { FC } from 'react';
+import styled from '@emotion/styled';
+import { CircularProgress } from '@mui/material';
+
+const ModalLoadingCircular: FC = () => {
+  return (
+    <Block>
+      <CircularProgress size={100}/>
+    </Block>
+  );
+};
+
+export default ModalLoadingCircular;
+
+const Block = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  min-height: 100vh;
+  background-color: rgba(0, 0, 0, 0.8);
+`;
