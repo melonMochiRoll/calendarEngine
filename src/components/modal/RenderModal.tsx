65eded7e0dd167789892778061f53534fbe28fd1
melonMochiRoll <soulhwasin@gmail.com>
2025-08-08 21:11:21 +0900

    :rewind: 모달 스플리팅 재적용

--- src/components/modal/RenderModal.tsx
+++ src/components/modal/RenderModal.tsx
@@ -3,28 +3,18 @@ import styled from '@emotion/styled';
 import { useAppDispatch, useAppSelector } from 'Hooks/reduxHooks';
 import { closeModal } from 'Features/modalSlice';
 import { ModalName } from 'Typings/types';
-import TodoDetail from './todo/TodoDetail';
-import TodoUpdate from './todo/TodoUpdate';
-import TodoInput from 'Components/modal/todo/TodoInput';
-import SharedspaceManagerModal from './sharedspaceManager/SharedspaceManagerModal';
-import SearchModal from './search/SearchModal';
-import SharedspaceMemberListModal from './sharedspaceMemberList/SharedspaceMemberListModal';
-import JoinRequestSenderModal from 'Components/modal/joinrequest/JoinRequestSenderModal';
-import JoinRequestManagerModal from 'Components/modal/joinrequest/JoinRequestManagerModal';
-import JoinRequestDetail from 'Components/modal/joinrequest/JoinRequestDetail';
-import ImageViewer from './imageViewer/ImageViewer';
 
-const modals = {
-  [ModalName.SEARCH]: SearchModal,
-  [ModalName.SHAREDSPACEMANAGER]: SharedspaceManagerModal,
-  [ModalName.TODO_INPUT]: TodoInput,
-  [ModalName.TODO_DETAIL]: TodoDetail,
-  [ModalName.TODO_UPDATE]: TodoUpdate,
-  [ModalName.SHAREDSPACEMEMBERLIST]: SharedspaceMemberListModal,
-  [ModalName.JOINREQUEST_SENDER]: JoinRequestSenderModal,
-  [ModalName.JOINREQUEST_MANAGER]: JoinRequestManagerModal,
-  [ModalName.JOINREQUEST_DETAIL]: JoinRequestDetail,
-  [ModalName.IMAGE_VIEWER]: ImageViewer,
+export const modals = {
+  [ModalName.SEARCH]: React.lazy(() => import('Components/modal/search/SearchModal')),
+  [ModalName.SHAREDSPACEMANAGER]: React.lazy(() => import('Components/modal/sharedspaceManager/SharedspaceManagerModal')),
+  [ModalName.TODO_INPUT]: React.lazy(() => import('Components/modal/todo/TodoInput')),
+  [ModalName.TODO_DETAIL]: React.lazy(() => import('Components/modal/todo/TodoDetail')),
+  [ModalName.TODO_UPDATE]: React.lazy(() => import('Components/modal/todo/TodoUpdate')),
+  [ModalName.SHAREDSPACEMEMBERLIST]: React.lazy(() => import('Components/modal/sharedspaceMemberList/SharedspaceMemberListModal')),
+  [ModalName.JOINREQUEST_SENDER]: React.lazy(() => import('Components/modal/joinrequest/JoinRequestSenderModal')),
+  [ModalName.JOINREQUEST_MANAGER]: React.lazy(() => import('Components/modal/joinrequest/JoinRequestManagerModal')),
+  [ModalName.JOINREQUEST_DETAIL]: React.lazy(() => import('Components/modal/joinrequest/JoinRequestDetail')),
+  [ModalName.IMAGE_VIEWER]: React.lazy(() => import('Components/modal/imageViewer/ImageViewer')),
 };
 
 const RenderModal: FC = () => {
@@ -35,7 +25,7 @@ const RenderModal: FC = () => {
 
   return (
     modalStack.map((modal, i) => {
-      const ModalComponent = modals[modal.name] as React.ComponentType<any>;
+      const ModalComponent = modals[modal.name] as React.LazyExoticComponent<React.ComponentType<any>>;
 
       return (
         <Backdrop
