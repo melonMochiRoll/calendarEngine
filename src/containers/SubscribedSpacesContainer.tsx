a34f167cb9b3b1f49a70cf13db5ce9f462286e8a
melonMochiRoll <soulhwasin@gmail.com>
2025-10-02 18:35:43 +0900

    :art: 페이지네이션 추가 및 일부 명칭 수정

--- src/containers/SubscribedSpacesContainer.tsx
+++ src/containers/SubscribedSpacesContainer.tsx
@@ -1,25 +1,27 @@
-import React, { FC, useState } from 'react';
+import React, { FC } from 'react';
 import styled from '@emotion/styled';
 import useUser from 'Hooks/queries/useUser';
 import { useSubscribedspace } from 'Hooks/queries/useSubscribedspaces';
 import SubscribedSpacesResult from 'Components/subscribedspaces/SubscribedspacesResult';
 import SubscribedSpacesHeader from 'Components/subscribedspaces/SubscribedspacesHeader';
 import SubscribedSpacesNull from 'Components/subscribedspaces/SubscribedSpacesNull';
-import { SubscribedspacesSortOptions } from 'Typings/types';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, useSearchParams } from 'react-router-dom';
 import { createSharedspace, deleteSharedspace } from 'Api/sharedspacesApi';
 import { PATHS } from 'Constants/paths';
 import { useQueryClient } from '@tanstack/react-query';
 import { GET_SUBSCRIBED_SPACES_KEY } from 'Constants/queryKeys';
 import { toast } from 'react-toastify';
 import { defaultToastOption, waitingMessage } from 'Constants/notices';
+import SubscribedSpacesPagination from 'Src/components/subscribedspaces/SubscribedSpacesPagination';
 
 const SubscribedSpacesContainer: FC = () => {
   const navigate = useNavigate();
+  const [ searchParams, setSearchParams ] = useSearchParams();
   const qc = useQueryClient();
-  const [ option, setOption ] = useState(SubscribedspacesSortOptions[0]);
+  const sort = searchParams.get('sort') || 'all';
+  const currentPage = Number(searchParams.get('page')) || 1;
 
-  const { data: subscribedspaceData } = useSubscribedspace(option.filter);
+  const { data: subscribedspaceData } = useSubscribedspace(sort, currentPage);
   const { data: userData } = useUser({ suspense: true, throwOnError: true });
 
   const onCreateSharedspace = (UserId: number) => {
@@ -37,7 +39,7 @@ const SubscribedSpacesContainer: FC = () => {
   const onDeleteSharedspace = (url: string) => {
     deleteSharedspace(url)
       .then(async () => {
-        await qc.refetchQueries([GET_SUBSCRIBED_SPACES_KEY]);
+        await qc.refetchQueries([GET_SUBSCRIBED_SPACES_KEY, sort, currentPage]);
       })
       .catch(() => {
         toast.error(waitingMessage, {
@@ -46,21 +48,29 @@ const SubscribedSpacesContainer: FC = () => {
       });
   };
 
-  const filterSpaces = (option: typeof SubscribedspacesSortOptions[0]) => {
-    setOption(option);
+  const sortSpaces = (sort: string) => {
+    setSearchParams({ sort });
+  };
+
+  const goToPage = (page: number) => {
+    setSearchParams({ sort, page: String(page) });
   };
 
   return (
     <Main>
       <SubscribedSpacesHeader
         onCreateSharedspace={() => onCreateSharedspace(userData.id)}
-        optionText={option.text}
-        filterSpaces={filterSpaces} />
-      {subscribedspaceData?.length ?
+        sort={sort}
+        sortSpaces={sortSpaces} />
+      {subscribedspaceData.spaces.length ?
         <SubscribedSpacesResult
-          subscribedspaceData={subscribedspaceData}
+          spaces={subscribedspaceData.spaces}
           onDeleteSharedspace={onDeleteSharedspace} /> :
         <SubscribedSpacesNull />}
+      <SubscribedSpacesPagination
+        currentPage={currentPage}
+        totalCount={subscribedspaceData.totalCount}
+        goToPage={goToPage} />
     </Main>
   );
 };
