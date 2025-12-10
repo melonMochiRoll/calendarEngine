ecfdf9afd16e494341f25d51021bd13826df5ac9
melonMochiRoll <soulhwasin@gmail.com>
2025-12-10 22:08:54 +0900

    :art: 전체 레이아웃을 ErrorBoundary로 감싸기

--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -5,18 +5,20 @@ import "react-toastify/dist/ReactToastify.css";
 import RenderModal from 'Components/modal/RenderModal';
 import { useCsrfToken } from 'Src/hooks/queries/useCsrfToken';
 import ModalLoadingCircular from 'Src/components/skeleton/ModalLoadingCircular';
+import { ErrorBoundary } from 'react-error-boundary';
+import GlobalErrorFallback from 'Src/components/errors/GlobalErrorFallback';
 
 const Layout: FC = () => {
   useCsrfToken();
 
   return (
-    <>
+    <ErrorBoundary fallbackRender={GlobalErrorFallback}>
       <Outlet />
       <ToastContainer />
       <Suspense fallback={<ModalLoadingCircular />}>
         <RenderModal />
       </Suspense>
-    </>
+    </ErrorBoundary>
   );
 };
 
