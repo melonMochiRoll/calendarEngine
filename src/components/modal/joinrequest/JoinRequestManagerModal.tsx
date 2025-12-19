0ea454e6329221a6209da1e7c7a03e38eda57845
melonMochiRoll <soulhwasin@gmail.com>
2025-12-19 16:57:59 +0900

    :recycle: ErrorBoundary, Suspense 적용 및 fallbackUI 수정

--- src/components/modal/joinrequest/JoinRequestManagerModal.tsx
+++ src/components/modal/joinrequest/JoinRequestManagerModal.tsx
@@ -1,32 +1,44 @@
-import React, { FC } from 'react';
-import styled from '@emotion/styled';
-import JoinRequestManagerHeader from './JoinRequestManagerHeader';
+import React, { FC, Suspense } from 'react';
 import JoinRequestManagerMain from './JoinRequestManagerMain';
-import AsyncBoundary from 'Components/AsyncBoundary';
-import LoadingCircular from 'Components/skeleton/LoadingCircular';
-import SharedspaceManagerError from '../sharedspaceManager/SharedspaceManagerError';
+import { ErrorBoundary } from 'react-error-boundary';
+import ModalLoadingCircular from 'Src/components/async/skeleton/ModalLoadingCircular';
+import ModalFallback from 'Src/components/async/fallbackUI/ModalFallback';
+import { useAppDispatch } from 'Src/hooks/reduxHooks';
+import { closeModal } from 'Src/features/modalSlice';
+import { useParams } from 'react-router-dom';
+import { useQueryClient } from '@tanstack/react-query';
+import { GET_JOINREQUEST_KEY } from 'Src/constants/queryKeys';
 
-const JoinRequestManagerModal: FC = () => { 
+interface JoinRequestManagerModalProps {
+  idx: number,
+  title: string,
+};
+
+const JoinRequestManagerModal: FC<JoinRequestManagerModalProps> = ({
+  idx,
+  title,
+}) => {
+  const { url } = useParams();
+  const qc = useQueryClient();
+  const dispatch = useAppDispatch();
+
+  function handleClose() {
+    dispatch(closeModal());
+  }
+
+  function removeQueries() {
+    qc.removeQueries([GET_JOINREQUEST_KEY, url]);
+  }
+
   return (
-    <Block onClick={e => e.stopPropagation()}>
-      <JoinRequestManagerHeader />
-      <AsyncBoundary
-        errorRenderComponent={<SharedspaceManagerError message={'에러가 발생했습니다.'} />}
-        suspenseFallback={<LoadingCircular />}>
-        <JoinRequestManagerMain />
-      </AsyncBoundary>
-    </Block>
+    <ErrorBoundary fallbackRender={(props) => ModalFallback(props, idx, title, handleClose, url, removeQueries)}>
+      <Suspense fallback={<ModalLoadingCircular idx={idx} handleClose={handleClose}/>}>
+        <JoinRequestManagerMain
+          idx={idx}
+          title={title} />
+      </Suspense>
+    </ErrorBoundary>
   );
 };
 
 export default JoinRequestManagerModal;
-
-const Block = styled.div`
-  display: flex;
-  flex-direction: column;
-  width: 650px;
-  height: 500px;
-  border-radius: 15px;
-  background-color: var(--black);
-  box-shadow: 1px 1px 10px 2px #000;
-`;
