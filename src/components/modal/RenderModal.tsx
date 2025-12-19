e9c9b6df900c9010f57eaae4d6bf51eaf080e81c
melonMochiRoll <soulhwasin@gmail.com>
2025-12-19 16:56:13 +0900

    :art: modals에서 모달 title을 주입하도록 수정

--- src/components/modal/RenderModal.tsx
+++ src/components/modal/RenderModal.tsx
@@ -3,16 +3,46 @@ import { useAppSelector } from 'Hooks/reduxHooks';
 import { ModalName } from 'Typings/types';
 
 export const modals = {
-  [ModalName.SEARCH]: React.lazy(() => import('Components/modal/search/SearchModal')),
-  [ModalName.SHAREDSPACEMANAGER]: React.lazy(() => import('Components/modal/sharedspaceManager/SharedspaceManagerModal')),
-  [ModalName.TODO_INPUT]: React.lazy(() => import('Components/modal/todo/TodoInput')),
-  [ModalName.TODO_DETAIL]: React.lazy(() => import('Components/modal/todo/TodoDetail')),
-  [ModalName.TODO_UPDATE]: React.lazy(() => import('Components/modal/todo/TodoUpdate')),
-  [ModalName.SHAREDSPACEMEMBERLIST]: React.lazy(() => import('Components/modal/sharedspaceMemberList/SharedspaceMemberListModal')),
-  [ModalName.JOINREQUEST_SENDER]: React.lazy(() => import('Components/modal/joinrequest/JoinRequestSenderModal')),
-  [ModalName.JOINREQUEST_MANAGER]: React.lazy(() => import('Components/modal/joinrequest/JoinRequestManagerModal')),
-  [ModalName.JOINREQUEST_DETAIL]: React.lazy(() => import('Components/modal/joinrequest/JoinRequestDetail')),
-  [ModalName.IMAGE_VIEWER]: React.lazy(() => import('Components/modal/imageViewer/ImageViewer')),
+  [ModalName.SEARCH]: {
+    title: '',
+    component: React.lazy(() => import('Components/modal/search/SearchModal')),
+  },
+  [ModalName.SHAREDSPACEMANAGER]: {
+    title: '',
+    component: React.lazy(() => import('Components/modal/sharedspaceManager/SharedspaceManagerModal')),
+  },
+  [ModalName.TODO_INPUT]: {
+    title: 'Todo 작성',
+    component: React.lazy(() => import('Components/modal/todo/TodoInput')),
+  },
+  [ModalName.TODO_DETAIL]: {
+    title: 'Todo',
+    component: React.lazy(() => import('Components/modal/todo/TodoDetail')),
+  },
+  [ModalName.TODO_UPDATE]: {
+    title: 'Todo 수정',
+    component: React.lazy(() => import('Components/modal/todo/TodoUpdate')),
+  },
+  [ModalName.SHAREDSPACEMEMBERLIST]: {
+    title: '멤버 목록',
+    component: React.lazy(() => import('Components/modal/sharedspaceMemberList/SharedspaceMemberListModal'))
+  },
+  [ModalName.JOINREQUEST_SENDER]: {
+    title: '스페이스 액세스 권한 요청',
+    component: React.lazy(() => import('Components/modal/joinrequest/JoinRequestSenderModal'))
+  },
+  [ModalName.JOINREQUEST_MANAGER]: {
+    title: '액세스 권한 요청 목록',
+    component: React.lazy(() => import('Components/modal/joinrequest/JoinRequestManagerModal')),
+  },
+  [ModalName.JOINREQUEST_DETAIL]: {
+    title: '',
+    component: React.lazy(() => import('Components/modal/joinrequest/JoinRequestDetail')),
+  },
+  [ModalName.IMAGE_VIEWER]: {
+    title: '',
+    component: React.lazy(() => import('Components/modal/imageViewer/ImageViewer')),
+  },
 };
 
 const RenderModal: FC = () => {
@@ -21,11 +51,16 @@ const RenderModal: FC = () => {
   if (!modalStack.length) return null;
 
   return (
-    modalStack.map((modal, i) => {
-      const ModalComponent = modals[modal.name] as React.LazyExoticComponent<React.ComponentType<any>>;
+    modalStack.map((modal, idx) => {
+      const ModalComponent = modals[modal.name].component as React.LazyExoticComponent<React.ComponentType<any>>;
+      const props = {
+        ...modal.props,
+        idx,
+        title: modals[modal.name].title,
+      };
 
       return (
-        <ModalComponent { ...{ ...modal.props, idx: i } }/>
+        <ModalComponent key={modal.name} { ...props } />
       );
     })
   );
