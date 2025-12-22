2198b4bb205137281cdaa3e953506b85459406e0
melonMochiRoll <soulhwasin@gmail.com>
2025-12-22 16:53:20 +0900

    :recycle: 모달 호출시 전달받을 props를 payload 프로퍼티로 묶음, BaseModalProps extends 적용

---
+++ src/components/modal/joinrequest/joinrequestManager/JoinRequestDetail.tsx
@@ -0,0 +1,122 @@
+import React, { FC } from 'react';
+import styled from '@emotion/styled';
+import CloseIcon from '@mui/icons-material/CloseRounded';
+import { useAppDispatch } from 'Hooks/reduxHooks';
+import { BaseModalProps, TJoinRequest } from 'Typings/types';
+import { closeModal } from 'Features/modalSlice';
+
+export interface JoinRequestDetailProps extends BaseModalProps {
+  payload: {
+    request: TJoinRequest,
+  },
+};
+
+const JoinRequestDetail: FC<JoinRequestDetailProps> = ({
+  payload,
+  idx,
+}) => {
+  const dispatch = useAppDispatch();
+  const { request } = payload;
+  
+  return (
+    <Backdrop
+      zIndex={100 + idx}
+      isBottom={!idx}
+      onClick={() => dispatch(closeModal())}>
+      <Block onClick={e => e.stopPropagation()}>
+        <Header>
+          <Left></Left>
+          <Center>
+            <ModalTitle>{request.Requestor.email}</ModalTitle>
+          </Center>
+          <Right>
+            <CloseIcon
+              onClick={() => dispatch(closeModal())}
+              sx={CloseIconInlineStyle} />
+          </Right>
+        </Header>
+        <Body>
+          <Span>{request.message}</Span>
+        </Body>
+      </Block>
+    </Backdrop>
+  );
+};
+
+export default JoinRequestDetail;
+
+const Backdrop = styled.div<{ zIndex: number, isBottom: boolean }>`
+  position: fixed;
+  inset: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  min-height: 100vh;
+  background-color: ${({ isBottom }) => isBottom ? 'rgba(0, 0, 0, 0.8)' : ''};
+  z-index: ${({ zIndex }) => zIndex};
+`;
+
+const Block = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 450px;
+  min-height: 300px;
+  border-radius: 15px;
+  background-color: var(--black);
+  box-shadow: 1px 1px 5px 1px #000;
+`;
+
+const Header = styled.header`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+  height: 60px;
+  padding: 20px 0;
+  border-bottom: 1px solid var(--light-gray);
+`;
+
+const Left = styled.div`
+  width: 15%;
+`;
+
+const Center = styled.div`
+  display: flex;
+  justify-content: center;
+  width: 70%;
+`;
+
+const ModalTitle = styled.h1`
+  color: var(--white);
+  font-size: 24px;
+  font-weight 600;
+  margin: 0;
+`;
+
+const Right = styled.div`
+  display: flex;
+  justify-content: center;
+  width: 15%;
+`;
+
+const Body = styled.section`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  height: 80%;
+  padding: 20px;
+  color: var(--white);
+`;
+
+const Span = styled.span`
+  font-size: 20px;
+  word-wrap: break-all;
+  word-break: break-all;
+`;
+
+const CloseIconInlineStyle = {
+  color: 'var(--white)',
+  fontSize: '35px',
+  cursor: 'pointer',
+};
