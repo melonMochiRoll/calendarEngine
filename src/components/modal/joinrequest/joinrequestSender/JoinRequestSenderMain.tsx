f1b40ff6dadfeaca7c981cbbf938887ab1a3bb00
melonMochiRoll <soulhwasin@gmail.com>
2025-12-24 14:41:44 +0900

    :recycle: Backdrop을 모달 내부에 정의, idx, title 프로퍼티 props 추가

--- src/components/modal/joinrequest/joinrequestSender/JoinRequestSenderMain.tsx
+++ src/components/modal/joinrequest/joinrequestSender/JoinRequestSenderMain.tsx
@@ -2,43 +2,91 @@ import React, { FC } from 'react';
 import styled from '@emotion/styled';
 import useInput from 'Hooks/utils/useInput';
 import TextButton from 'Components/common/TextButton';
+import { useAppDispatch } from 'Src/hooks/reduxHooks';
+import { closeModal } from 'Src/features/modalSlice';
+import MailIcon from '@mui/icons-material/MarkEmailRead';
+import CloseIcon from '@mui/icons-material/CloseRounded';
 
 interface JoinRequestSenderMainProps {
-  onSubmit: (message: string) => void;
-  error: string;
+  onSubmit: (message: string) => void,
+  idx: number,
+  title: string,
+  error: string,
 };
 
 const JoinRequestSenderMain: FC<JoinRequestSenderMainProps> = ({
   onSubmit,
+  idx,
+  title,
   error,
 }) => {
+  const dispatch = useAppDispatch();
   const [ message, onChangeMessage ] = useInput('');
 
   return (
-    <>
-      <Main>
-        <TextField
-          value={message}
-          onChange={onChangeMessage}
-          placeholder='메세지' />
-      </Main>
-      <Footer>
-        <ErrorSpan>{error}</ErrorSpan>
-        <Buttons>
-          <TextButton
-            type='button'
-            onClick={() => {
-              onSubmit(message);
-            }}>
-              전송
-          </TextButton>
-        </Buttons>
-      </Footer>
-    </>
+    <Backdrop
+      zIndex={100 + idx}
+      isBottom={!idx}
+      onClick={() => dispatch(closeModal())}>
+      <Block onClick={e => e.stopPropagation()}>
+        <Header>
+          <Left></Left>
+          <Center>
+            <MailIcon fontSize='large' />
+            <ModalTitle>{title}</ModalTitle>
+          </Center>
+          <Right>
+            <CloseIcon
+              onClick={() => dispatch(closeModal())}
+              sx={CloseIconInlineStyle} />
+          </Right>
+        </Header>
+        <Main>
+          <TextField
+            value={message}
+            onChange={onChangeMessage}
+            placeholder='메세지' />
+        </Main>
+        <Footer>
+          <ErrorSpan>{error}</ErrorSpan>
+          <Buttons>
+            <TextButton
+              type='button'
+              onClick={() => {
+                onSubmit(message);
+              }}>
+                전송
+            </TextButton>
+          </Buttons>
+        </Footer>
+      </Block>
+    </Backdrop>
   );
 };
 
 export default JoinRequestSenderMain;
+
+const Backdrop = styled.div<{ zIndex: number, isBottom: boolean }>`
+  position: fixed;
+  inset: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  min-height: 100vh;
+  background-color: ${({ isBottom }) => isBottom ? 'rgba(0, 0, 0, 0.8)' : ''};
+  z-index: ${({ zIndex }) => zIndex};
+`;
+
+const Block = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 550px;
+  height: 350px;
+  border-radius: 15px;
+  background-color: var(--black);
+  box-shadow: 1px 1px 10px 2px #000;
+`;
 
 const Main = styled.div`
   display: flex;
@@ -79,3 +127,45 @@ const ErrorSpan = styled.span`
   font-size: 16px;
   color: var(--red);
 `;
+
+const Header = styled.header`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+  height: 20%;
+  padding: 20px 0;
+  border-bottom: 1px solid var(--light-gray);
+`;
+
+const Left = styled.div`
+  width: 15%;
+`;
+
+const Center = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: var(--white);
+  width: 70%;
+  gap: 15px;
+`;
+
+const ModalTitle = styled.h1`
+  color: var(--white);
+  font-size: 24px;
+  font-weight 600;
+  margin: 0;
+`;
+
+const Right = styled.div`
+  display: flex;
+  justify-content: center;
+  width: 15%;
+`;
+
+const CloseIconInlineStyle = {
+  color: 'var(--white)',
+  fontSize: '35px',
+  cursor: 'pointer',
+};
