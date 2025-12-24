9f90bd79c7f7730c6bc543f783d9be849f21045f
melonMochiRoll <soulhwasin@gmail.com>
2025-12-24 15:19:08 +0900

    :recycle: Backdrop, query state, 스타일 컴포넌트를 Main에 정의, BaseModalProps extends 적용

--- src/components/modal/sharedspaceManager/SharedspaceManagerModal.tsx
+++ src/components/modal/sharedspaceManager/SharedspaceManagerModal.tsx
@@ -1,39 +1,40 @@
-import React, { FC, useState } from 'react';
-import styled from '@emotion/styled';
+import React, { FC, Suspense } from 'react';
 import SharedspaceManagerMain from './SharedspaceManagerMain';
-import AsyncBoundary from 'Components/AsyncBoundary';
-import SharedspaceManagerHeader from './SharedspaceManagerHeader';
-import SharedspaceManagerError from './SharedspaceManagerError';
-import LoadingCircular from 'Components/skeleton/LoadingCircular';
-import { useDebounce } from 'Hooks/utils/useDebounce';
+import { ErrorBoundary } from 'react-error-boundary';
+import { BaseModalProps } from 'Src/typings/types';
+import ModalLoadingCircular from 'Src/components/async/skeleton/ModalLoadingCircular';
+import { useParams } from 'react-router-dom';
+import { useQueryClient } from '@tanstack/react-query';
+import { useAppDispatch } from 'Src/hooks/reduxHooks';
+import { closeModal } from 'Src/features/modalSlice';
+import ModalFallback from 'Src/components/async/fallbackUI/ModalFallback';
+import { SEARCH_USERS_KEY } from 'Src/constants/queryKeys';
 
-const SharedspaceManagerModal: FC = () => {
-  const [ query, setQuery ] = useState('');
-  const debouncedQuery = useDebounce(query, 500);
+interface SharedspaceManagerModalProps extends BaseModalProps {};
+
+const SharedspaceManagerModal: FC<SharedspaceManagerModalProps> = ({
+  idx,
+  title,
+}) => {
+  const { url } = useParams();
+  const qc = useQueryClient();
+  const dispatch = useAppDispatch();
   
+  function handleClose() {
+    dispatch(closeModal());
+  }
+
+  function removeQueries() {
+    qc.removeQueries([SEARCH_USERS_KEY, url]);
+  }
+
   return (
-    <Block onClick={e => e.stopPropagation()}>
-      <SharedspaceManagerHeader
-        query={query}
-        setQuery={setQuery} />
-      <AsyncBoundary
-        errorRenderComponent={<SharedspaceManagerError message={'에러가 발생했습니다.'} />}
-        suspenseFallback={<LoadingCircular />}>
-        <SharedspaceManagerMain query={debouncedQuery}/>
-      </AsyncBoundary>
-    </Block>
+    <ErrorBoundary fallbackRender={(props) => ModalFallback(props, idx, title, handleClose, url, removeQueries)}>
+      <Suspense fallback={<ModalLoadingCircular idx={idx} handleClose={handleClose}/>}>
+        <SharedspaceManagerMain idx={idx} />
+      </Suspense>
+    </ErrorBoundary>
   );
 };
 
 export default SharedspaceManagerModal;
-
-const Block = styled.div`
-  display: flex;
-  flex-direction: column;
-  width: 650px;
-  height: 500px;
-  border: 1px solid #1d2126;
-  border-radius: 15px;
-  background-color: var(--black);
-  box-shadow: 1px 1px 10px 2px #000;
-`;
