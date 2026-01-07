a71cd0f8b78af509cbf61778ca0f6c2900d4b9d1
melonMochiRoll <soulhwasin@gmail.com>
2026-01-07 12:55:38 +0900

    :recycle: 구조 개선

--- src/layouts/SharedspacesLayout.tsx
+++ src/layouts/SharedspacesLayout.tsx
@@ -1,17 +1,46 @@
 import React, { FC } from 'react';
-import { ErrorBoundary } from 'react-error-boundary';
-import GlobalErrorFallback from 'Src/components/errors/GlobalErrorFallback';
-import SharedspaceContainer from 'Src/containers/SharedspaceContainer';
+import styled from '@emotion/styled';
+import { Outlet } from 'react-router-dom';
+import Drawer from 'Components/common/Drawer';
+import TodoContainer from 'Containers/TodoContainer';
+import Sidebar from 'Src/layouts/Sidebar';
+import SharedspaceHeader from 'Src/layouts/SharedspaceHeader';
 
 interface SharedspacesLayoutProps {};
 
 const SharedspacesLayout: FC<SharedspacesLayoutProps> = ({}) => {
   return (
-    <ErrorBoundary
-      fallbackRender={GlobalErrorFallback}>
-      <SharedspaceContainer />
-    </ErrorBoundary>
+    <Block>
+      <Sidebar />
+      <PageWrapper>
+        <SharedspaceHeader />
+        <ContentWrapper>
+          <Outlet />
+          <Drawer>
+            <TodoContainer />
+          </Drawer>
+        </ContentWrapper>
+      </PageWrapper>
+    </Block>
   );
 };
 
 export default SharedspacesLayout;
+
+const Block = styled.div`
+  display: flex;
+`;
+
+const PageWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  height: 100vh;
+  background-color: var(--black);
+`;
+
+const ContentWrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  height: 95%;
+`;
