721b7338cc0f1b1f4066af10b27e80657e2eed9f
melonMochiRoll <soulhwasin@gmail.com>
2026-02-05 15:50:00 +0900

    :lipstick: Header 의존성 추가

--- src/pages/SharedspacesPage.tsx
+++ src/pages/SharedspacesPage.tsx
@@ -1,20 +1,31 @@
 import React, { FC, Suspense } from 'react';
+import styled from '@emotion/styled';
 import SubscribedSpacesContainer from 'Containers/SubscribedSpacesContainer';
 import { ErrorBoundary } from 'react-error-boundary';
 import RequireLogin from 'Src/components/guard/RequireLogin';
 import SharedspaceFallback from 'Src/components/async/fallbackUI/SharedspaceFallback';
 import LoadingPage from 'Src/components/async/skeleton/LoadingPage';
+import Header from 'Src/layouts/Header';
 
 const SharedspacesPage: FC = () => {
   return (
-    <ErrorBoundary fallbackRender={(props) => <SharedspaceFallback errorProps={props} />}>
-      <Suspense fallback={<LoadingPage />}>
-        <RequireLogin>
-          <SubscribedSpacesContainer />
-        </RequireLogin>
-      </Suspense>
-    </ErrorBoundary>
+    <Block>
+      <Header />
+      <ErrorBoundary fallbackRender={(props) => <SharedspaceFallback errorProps={props} />}>
+        <Suspense fallback={<LoadingPage />}>
+          <RequireLogin>
+            <SubscribedSpacesContainer />
+          </RequireLogin>
+        </Suspense>
+      </ErrorBoundary>
+    </Block>
   );
 };
 
 export default SharedspacesPage;
+
+const Block = styled.div`
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+`;
