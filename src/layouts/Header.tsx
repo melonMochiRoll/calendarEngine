556a4443b53b581cf37bdaf47d0397c65f81f262
melonMochiRoll <soulhwasin@gmail.com>
2026-02-05 16:06:16 +0900

    :lipstick: 받은 초대 모달 호출 버튼 추가

--- src/layouts/Header.tsx
+++ src/layouts/Header.tsx
@@ -1,34 +1,34 @@
 import React, { FC, Suspense } from 'react';
 import styled from '@emotion/styled';
 import RenderUserProfile from 'Components/auth/RenderUserProfile';
-import SatelliteIcon from '@mui/icons-material/SatelliteAlt';
-import { useNavigate } from 'react-router-dom';
-import { PATHS } from 'Constants/paths';
 import SkeletonUserProfile from 'Src/components/async/skeleton/SkeletonUserProfile';
+import TextButton from 'Src/components/common/TextButton';
+import { useAppDispatch } from 'Src/hooks/reduxHooks';
+import { ModalName } from 'Src/typings/types';
+import { openModal } from 'Src/features/modalSlice';
 
 const Header: FC = () => {
-  const navigate = useNavigate();
-  
+  const dispatch = useAppDispatch();
+
   return (
-    <HeaderBlock>
-      <InfoWrapper>
-        <FlexBox>
-          <SatelliteIcon
-            onClick={() => navigate(PATHS.SHAREDSPACE)}
-            fontSize='large'
-            sx={{ color: 'var(--blue)', cursor: 'pointer', marginRight: '10px' }}/>
-        </FlexBox>
-      </InfoWrapper>
+    <Block>
+      <TextButton
+        type='button'
+        onClick={() => {
+          dispatch(openModal({ name: ModalName.SHAREDSPACE_INVITE_RECEIVED }));
+        }}>
+        받은 초대
+      </TextButton>
       <Suspense fallback={<SkeletonUserProfile />}>
         <RenderUserProfile />
       </Suspense>
-    </HeaderBlock>
+    </Block>
   );
 };
 
 export default Header;
 
-const HeaderBlock = styled.header`
+const Block = styled.header`
   display: flex;
   justify-content: space-between;
   align-items: center;
@@ -37,17 +37,4 @@ const HeaderBlock = styled.header`
   padding: 10px 5%;
   border-bottom: 1px solid var(--light-gray);
   background-color: var(--black);
-`;
-
-const InfoWrapper = styled.div`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  width: 70%;
-`;
-
-const FlexBox = styled.div`
-  display: flex;
-  align-items: center;
-  gap: 10px;
 `;
