669ad055799884a205e83dd82aa88cb142c4c3c3
melonMochiRoll <soulhwasin@gmail.com>
2026-02-10 17:00:49 +0900

    :sparkles: getInvites 정의

--- src/api/inviteApi.ts
+++ src/api/inviteApi.ts
@@ -1,5 +1,18 @@
 import { axiosInstance } from "./axiosInstance";
 
+export const getInvites = async (
+  page: number,
+) => {
+  try {
+    const { data } = await axiosInstance
+      .get(`api/invites?page=${page}`);
+
+    return data;
+  } catch (err) {
+    throw err;
+  }
+};
+
 export const sendInvite = async (
   url: string | undefined,
   inviteeEmail: string,
