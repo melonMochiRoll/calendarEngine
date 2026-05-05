1596b5d4fa6df696d188f966ec09e9efc2a7f917
melonMochiRoll <soulhwasin@gmail.com>
2026-05-05 14:56:38 +0900

    :sparkles: access token 만료시 refresh 요청을 보내도록 하는 인터셉터 정의

--- src/api/axiosInstance.ts
+++ src/api/axiosInstance.ts
@@ -1,5 +1,7 @@
-import axios from "axios";
+import axios, { AxiosError } from "axios";
 import { reduxStore } from "Src/store";
+import { refreshAuthToken } from "./authApi";
+import { ERROR_TYPE } from "Src/constants/constants";
 
 export const axiosInstance = axios.create({
   baseURL: process.env.REACT_APP_SERVER_ORIGIN,
@@ -18,3 +20,22 @@ axiosInstance.interceptors.request.use((config) => {
 }, error => {
   throw error;
 });
+
+axiosInstance.interceptors.response.use(
+  (res) => res,
+  async (err) => {
+    const { config } = err;
+
+    try {
+      if (err instanceof AxiosError && err?.response?.data?.metaData?.type === ERROR_TYPE.TOKEN_EXPIRED) {
+        await refreshAuthToken();
+
+        return axiosInstance(config);
+      }
+    } catch (refreshError) {
+      throw refreshError;
+    }
+
+    throw err;
+  }
+);
