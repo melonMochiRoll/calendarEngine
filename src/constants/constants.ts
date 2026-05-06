2a25b329f65843539ce77cc32cbad1eb5f25d677
melonMochiRoll <soulhwasin@gmail.com>
2026-05-06 16:50:10 +0900

    :sparkles: SocketStatus 정의

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -6,3 +6,10 @@ export const ERROR_TYPE = {
 export const ChatEmitEvent = {
   SEND_SHAREDSPACE_CHAT: 'send_sharedspace_chat',
 } as const;
+
+export const SocketStatus = {
+  CONNECTED: '연결됨',
+  DISCONNECTED: '연결 끊어짐',
+  CONNECTING: '연결 중',
+  RECONNECTING: '연결 시도 중',
+};
