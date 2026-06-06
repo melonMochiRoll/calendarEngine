ddc22fee2cdec8d44285bda6ea541ab18ccb3a9e
melonMochiRoll <soulhwasin@gmail.com>
2026-06-06 14:44:39 +0900

    :sparkles: 액세스 토큰 만료 직전 선제적으로 리프레쉬하도록하는 로직 추가

--- src/api/axiosInstance.ts
+++ src/api/axiosInstance.ts
@@ -3,6 +3,9 @@ import { reduxStore } from "Src/store";
 import { refreshAuthToken } from "./authApi";
 import { AUTHORIZATION_HEADER_NAME, CSRF_TOKEN_HEADER_NAME, ERROR_TYPE } from "Src/constants/constants";
 import { setAccessToken } from "Src/features/accessTokenSlice";
+import { jwtDecode } from "jwt-decode";
+import { TAccessTokenPayload } from "Src/typings/types";
+import dayjs from "dayjs";
 
 export const axiosInstance = axios.create({
   baseURL: process.env.REACT_APP_SERVER_ORIGIN,
@@ -10,16 +13,35 @@ export const axiosInstance = axios.create({
   withCredentials: true,
 });
 
-axiosInstance.interceptors.request.use((config) => {
+axiosInstance.interceptors.request.use(async (config) => {
   const accessToken = reduxStore.getState().accessToken.token;
   const csrfToken = reduxStore.getState().csrfToken.token;
 
-  if (accessToken) {
-    config.headers[AUTHORIZATION_HEADER_NAME] = `Bearer ${accessToken}`;
-  }
-
   if (csrfToken) {
     config.headers[CSRF_TOKEN_HEADER_NAME] = csrfToken;
+  }
+
+  if (accessToken) {
+    const accessTokenPayload = jwtDecode<TAccessTokenPayload>(accessToken);
+    const isExpired = dayjs().isSameOrAfter(dayjs(accessTokenPayload.exp, 'X'));
+
+    if (isExpired) {
+      try {
+        const { accessToken }: any = await axios
+          .post(
+            `${process.env.REACT_APP_SERVER_ORIGIN}/api/auth/refresh`,
+            {
+              headers: { "Content-Type" : "application/json" },
+              withCredentials: true,
+            }
+          );
+        reduxStore.dispatch(setAccessToken({ token: accessToken }));
+      } catch {
+        return config;
+      }
+    }
+
+    config.headers[AUTHORIZATION_HEADER_NAME] = `Bearer ${accessToken}`;
   }
 
   return config;
