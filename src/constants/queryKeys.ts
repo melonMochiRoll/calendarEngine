6216092dee5622fdea4cea3205d502b07ff7f72c
melonMochiRoll <soulhwasin@gmail.com>
2026-06-17 17:24:49 +0900

    :sparkles: GET_DIRECT_CHATS_KEY 정의

--- src/constants/queryKeys.ts
+++ src/constants/queryKeys.ts
@@ -8,5 +8,7 @@ export const GET_SHAREDSPACE_CHATS_KEY = 'getSharedspaceChats';
 export const GET_SHAREDSPACE_MEMBERS_KEY = 'getSharedspaceMembers';
 export const GET_INVITES_KEY = 'getInvites';
 
+export const GET_DIRECT_CHATS_KEY = 'getDirectChats';
+
 export const SEARCH_TODOS_KEY = 'searchTodos';
 export const SEARCH_USERS_KEY = 'searchUsers';
