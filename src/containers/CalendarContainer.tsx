1804e15fe57e8a9c8efff617e36e6809ed016c5b
melonMochiRoll <soulhwasin@gmail.com>
2026-06-17 14:03:19 +0900

    :recycle: width: 100% 대신 flex-grow: 1로 공간을 채우도록 수정

--- src/containers/CalendarContainer.tsx
+++ src/containers/CalendarContainer.tsx
@@ -43,8 +43,8 @@ export default CalendarContainer;
 const Background = styled.div`
   display: flex;
   flex-direction: column;
+  flex-grow: 1;
   flex-shrink: 1;
-  width: 100%;
   height: 100%;
   padding: 20px;
   background-color: var(--black);
