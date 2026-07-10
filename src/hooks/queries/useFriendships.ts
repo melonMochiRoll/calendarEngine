bfa241611ec390d1fa7fa9cb2734859527401719
melonMochiRoll <soulhwasin@gmail.com>
2026-07-10 17:29:27 +0900

    :sparkles: useFriendships 정의

---
+++ src/hooks/queries/useFriendships.ts
@@ -0,0 +1,45 @@
+import { useQuery, useQueryClient } from "@tanstack/react-query";
+import { useCallback } from "react";
+import { getFriendships } from "Src/api/friendshipsApi";
+import { GET_FRIENDSHIPS } from "Src/constants/queryKeys";
+import { handleRetry } from "Src/lib/utilFunction";
+import { TFriendshipResponse } from "Src/typings/types";
+
+export function useFriendships() {
+  const qc = useQueryClient();
+  
+  const {
+    data,
+    isLoading,
+    error,
+  } = useQuery<TFriendshipResponse>({
+    queryKey: [GET_FRIENDSHIPS],
+    queryFn: () => getFriendships(),
+    refetchOnWindowFocus: false,
+    suspense: true,
+    useErrorBoundary: true,
+    retry: (failureCount, error) => handleRetry([ 400, 401, 403, 404 ], failureCount, error),
+  });
+
+  if (isLoading) throw new Promise(() => {});
+  if (error) throw error;
+  if (data === null || data === undefined) throw new Error();
+
+  const loadMore = useCallback(async () => {
+    const moreFriendship = await getFriendships(data.friendships[data.friendships.length-1].RequesterId);
+
+    qc.setQueryData<TFriendshipResponse>([GET_FRIENDSHIPS], (prev) => {
+      if (!prev) return;
+
+      return {
+        friendships: [ ...prev.friendships || [], ...moreFriendship.chats ],
+        totalCount: prev.totalCount,
+      };
+    });
+  }, [data]);
+
+  return {
+    data,
+    loadMore,
+  } as const;
+}
